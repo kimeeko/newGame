d490345ccfc649ef8cd454c724e177cf90de78ed
kimeeko <kimirrrrra76@gmail.com>
2025-03-28 22:49:31 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,170 @@
+#include <fstream>
+#include "Header.h"
+#include <string>
+
+using namespace Nastolki;
+using namespace std;
+
+string ni::name() { return _name; }
+string ni::vid() { return _vid; }
+int ni::vozrast() { return _vozrast; }
+string ni::sloznost() { return _sloznost; }
+int ni::chislo_people() { return _chislo_people; }
+int ni::price() { return _price; }
+
+ni::ni(std::string name, std::string vid, int vozrast, std::string sloznost, int chislo_people, int price, ni* next, ni* first, ni* last, int n) : _name(name), _vid(vid), _vozrast(vozrast), _sloznost(sloznost), _chislo_people(chislo_people), _price(price), _next(next), _first(first), _last(last), _n(n) {}
+istream& Nastolki::operator >> (istream& in, ni& p) {
+    in >> p._name >> p._vid >> p._vozrast >> p._sloznost >> p._chislo_people >> p._price;
+    return in;
+}
+
+void Nastolki::push(ni* l, std::string name, std::string vid, int vozrast, std::string sloznost, int chislo_people, int price) {
+    ni* ne = new ni(name, vid, vozrast, sloznost, chislo_people, price);
+    ne->next = nullptr;
+    if (l->n == 0)
+    {
+        l->first = ne;
+    }
+    else
+    {
+        l->last->next = ne;
+    }
+    l->last = ne;
+    l->n++;
+}
+void Nastolki::load(ni* l, const string igri1)
+{
+    ifstream file(igri1);
+    if (!file)
+    {
+        cout << "файл не найден. Создайте новый файл" << endl;
+        ofstream newFile(igri1);
+        if (!file)
+        {
+            cout << "Ошибка! Не удалось создать файл." << endl;
+            return;
+        }
+        file.close();
+        return;
+    }
+    string name, vid, sloznost;
+    int vozrast, chislo_people, price;
+    while (file >> name >> vid >> vozrast >> sloznost >> chislo_people >> price)
+    {
+        push(l, name, vid, vozrast, sloznost, chislo_people, price);
+    }
+    file.close();
+}
+void Nastolki::save(ni* l, const string igri1)
+{
+    ofstream file(igri1);
+    if (!file)
+    {
+        cout << "Ошибка! Не удалось сохранить файл." << endl;
+        return;
+    }
+    ni* current = l->first;
+    while (current)
+    {
+        file << current->name() << " " << current->vid() << " " << current->vozrast() << " " << current->sloznost() << " " << current->chislo_people() << " " << current->price() << endl;
+        current = current->next;
+    }
+    file.close();
+}
+ni* Nastolki::get(ni* l, int index)
+{
+    if (index < 0 || index >= l->n)
+    {
+        return NULL;
+    }
+    ni* current = l->first;
+    for (int i = 0; i < index; i++)
+    {
+        current = current->next;
+    }
+    return current;
+}
+void Nastolki::deletes(ni* l, int index)
+{
+    if (l->n == 0 || index < 0 || index >= l->n)
+    {
+        cout << "Неверный индекс." << endl;
+        return;
+    }
+    if (index == 0)
+    {
+        ni* temp = l->first;
+        l->first = temp->next;
+        free(temp);
+    }
+    else
+    {
+        ni* prev = get(l, index - 1);
+        if (!prev)
+        {
+            cout << "Товар не найден." << endl;
+            return;
+        }
+        ni* current = prev->next;
+        if (current == l->last)
+        {
+            prev->next = NULL;
+            l->last = prev;
+        }
+        else
+        {
+            prev->next = current->next;
+        }
+        free(current);
+    }
+    l->n--;
+}
+void Nastolki::print(ni* l)
+{
+    ni* current = l->first;
+    int index = 1;
+    if (l -> first == nullptr) {
+        cout << "Товаров нет." << endl;
+    }
+    while (current)
+    {
+        cout << "Индекс: " << index << " " << "Имя: " << current->name() << " " << "Вид:  " << current->vid() << " " << "Возраст: " << current->vozrast() << " " << "Сложность: " << current->sloznost() << " " << "Количество игроков:  " << current->chislo_people() << " " << " Цена: " << current->price() << endl;
+        current = current->next;
+        index++;
+    }
+}
+void Nastolki::search(ni* l, string vid, int min_price)
+{
+    ni* current = l->first;
+    int found = 0;
+    while (current)
+    {
+        if (current->vid().compare(vid) == 0 && current->price() == min_price)
+        {
+            cout << "Найдено: Имя = " << current->name() << " " << "Цена: " << current->price() << endl;
+            found = 1;
+        }
+        current = current->next;
+    }
+    if (!found)
+    {
+        cout << "Товар не найден." << endl;
+    }
+}
+void Nastolki::edit(ni* l, int index)
+{
+    if (l->n == 0 || index < 0 || index >= l->n)
+    {
+        cout << "Неверный индекс." << endl;
+        return;
+    }
+
+    ni* current = get(l, index);
+    if (!current)
+    {
+        cout << "ERROR!" << endl;
+        return;
+    }
+    cout << "Введите Новое имя, Новый тип, Новый минимальный возраст игрока, Новую сложность, Новое количество игроков, Новую цену товара: ";
+    cin >> *current;
+}
---
+++ lab.cpp
@@ -0,0 +1,88 @@
+﻿#include <string>
+#include <locale.h>
+#include "Header.h"
+using namespace std;
+using namespace Nastolki;
+int main()
+{
+    char* locale = setlocale(LC_ALL, "Rus");
+    ni* l = new ni("", "", 0, "", 0, 0);
+    ni* first = NULL;
+    ni* last = NULL;
+    ni* n = 0;
+    load(l, "igri1");
+
+    int choice;
+    string vid;
+    int min_price;
+
+    do
+    {
+        cout << "Меню:" << endl;
+        cout << "1. Показать все товары" << endl;
+        cout << "2. Добавить новый товар" << endl;
+        cout << "3. Удалить товар по индексу" << endl;
+        cout << "4. Найти по типу и цене" << endl;
+        cout << "5. Редактировать товар по индексу" << endl;
+        cout << "6. Выход" << endl;
+        cout << "Введите ваш выбор: " << endl;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            print(l);
+            break;
+        case 2:
+        {
+            string name, sloznost, vid;
+            int vozrast, chislo_people, price;
+            cout << "Введите Имя, Тип, Минимальный возраст игрока, Сложность, Количество игроков, Цену товара: ";
+            cin >> name >> vid >> vozrast >> sloznost >> chislo_people >> price;
+            push(l, name, vid, vozrast, sloznost, chislo_people, price);
+            save(l, "igri1");
+        }
+        break;
+        case 3:
+            print(l);
+            {
+                int index;
+                cout << "Введите индекс товара для удаления: ";
+                cin >> index;
+                deletes(l, index);
+                save(l, "igri1");
+            }
+            break;
+        case 4:
+            cout << "Введите тип и цену товара: ";
+            cin >> vid >> min_price;
+            search(l, vid, min_price);
+            break;
+        case 5:
+            print(l);
+            {
+                int index;
+                cout << "Введите индекс товара для редактирования: ";
+                cin >> index;
+                edit(l, index);
+                save(l, "igri1");
+            }
+            break;
+        case 6:
+            cout << "Выход...";
+            break;
+        default:
+            cout << "Неверный выбор, введите число от 1 до 6.";
+        }
+    } while (choice != 6);
+
+    ni* current = l -> first;
+    while (current)
+    {
+        ni* temp = current->next;
+        delete (current);
+        current = temp;
+    }
+    return 0;
+}
+
+
